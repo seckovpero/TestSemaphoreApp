6be8edf5e7c4bc7bad03fc19ed0a772760a0951f
Pero Sechkov <perosechvko@Peros-MacBook-Pro.local>
2020-12-17 11:46:53 +0100

    Initial commit

---
+++ TestSemaphoreApp/MainScene/Models/Semaphore.swift
@@ -0,0 +1,8 @@
+//
+//  Semaphore.swift
+//  TestSemaphoreApp
+//
+//  Created by Pero Sechkov on 17.12.20.
+//
+
+import Foundation
---
+++ TestSemaphoreApp/MainScene/Models/SemaphoreDataModel.swift
@@ -0,0 +1,8 @@
+//
+//  SemaphoreDataModel.swift
+//  TestSemaphoreApp
+//
+//  Created by Pero Sechkov on 17.12.20.
+//
+
+import Foundation
