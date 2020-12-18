10e6d33a3478400687ffea9b6ec3dcd25edda2a0
Pero Sechkov <pero.sechkov@echoios.com>
2020-12-18 16:50:36 +0100

    Test

--- TestSemaphoreApp/MainScene/ViewController.swift
+++ TestSemaphoreApp/MainScene/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     
     //MARK: - SETUP NAVIGATION BAR
     func setupNavigationBar() {
-        self.title = "TEST Igor Auto Distribute"
+        self.title = "TEST Igor Auto Send"
         
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(deleteButtonTapped))
         
