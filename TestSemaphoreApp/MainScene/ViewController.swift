ef93a243293c25c0fae39bb53ac5573be9194bb5
Pero Sechkov <pero.sechkov@echoios.com>
2020-12-20 11:22:15 +0100

    Test Despina

--- TestSemaphoreApp/MainScene/ViewController.swift
+++ TestSemaphoreApp/MainScene/ViewController.swift
@@ -22,7 +22,7 @@ class ViewController: UIViewController {
     
     //MARK: - SETUP NAVIGATION BAR
     func setupNavigationBar() {
-        self.title = "Miki"
+        self.title = "Despina"
         
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .close, target: self, action: #selector(deleteButtonTapped))
         
